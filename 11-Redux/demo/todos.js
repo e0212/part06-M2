aa16145a4d2f1b654c16cba9bd32386f6edd09ed
unknown <villaemiliano274@gmail.com>
2022-05-23 02:09:37 -0300

    modificada todos

--- 11-Redux/demo/todos.js
+++ 11-Redux/demo/todos.js
@@ -1,15 +1,23 @@
+// importo el modulo redux
 const redux = require('redux');
 
+
+// traigo el metodo createStore del modulo de redux
 const createStore = redux.createStore;
 
+
+// crea constantes con los types de las acciones
 const ADD_TODO = 'ADD_TODO'
 const REMOVE_TODO = 'REMOVE_TODO';
 
 
+// define un estado inicial
 const initialState = {
-  todos: []
+  todos: [] // [{text: aa, completed: false}, {} ] ver filter de abajo (e, i)
 }
 
+
+// reducer
 const rootReducer = (state = initialState, action) => {
   switch(action.type) {
     case ADD_TODO:
@@ -18,36 +26,40 @@ const rootReducer = (state = initialState, action) => {
       }
     case REMOVE_TODO:
       return {
-        todos: state.todos.filter((text, i) => i !== action.payload)
+        todos: state.todos.filter((text, i) => i !== action.payload) // un nuevo array filtrado []
       }
     default:
       return state;
   }
 }
 
-const store = createStore(rootReducer);
+// crea el store
+const store = createStore(rootReducer); // recibe el reducer
 
-store.subscribe(() => {
+store.subscribe(() => { // subscribe es un metodo que se invoca cada vez que se dispacha una accion
   console.log('Subscription: ', store.getState());
 });
 
+// action creator
 function addTodo(text) {
   return {
     type: ADD_TODO,
-    payload: text
+    payload: text  // "repasar redux"
   }
 }
 
+//action creator
 function removeTodo(index) {
   return {
     type: REMOVE_TODO,
-    payload: index
+    payload: index // el payload es info extra
   }
 }
 
-store.dispatch(addTodo('Comprar pan'))
+store.dispatch(addTodo('Comprar pan'))// le pasa la action creator addTodo
+
 store.dispatch(addTodo('Correr'))
 
-store.dispatch(removeTodo(1))
+store.dispatch(removeTodo(1)) // elimina indice del array 1
 
 console.log(store.getState());
